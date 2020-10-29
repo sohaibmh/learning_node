d9d93752adaf68ad7c4738fb9505c2adb86f7317
Sohaib Mubashir <sohaibmh@yahoo.com>
2020-10-29 22:46:47 +0000

    added sendgrid configuration

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,11 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for your feedback!");
+  });
+
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     // creating a new instance of the Survey model
@@ -22,6 +26,16 @@ module.exports = (app) => {
       dateSent: Date.now(),
     });
 
-    const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
